5b515e8796f27b2e89ce5d09e95df2e2cbbc22a8
Caree Youngman <caree@caree.codes>
2021-10-28 18:32:23 -0500

    Adding event listener to deal button

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
 window.addEventListener('DOMContentLoaded', function() {
-  // Execute after page load
+  const dealBtn = document.querySelector("#deal-button");
+  
+  // Add a click event listener to the button
+  dealBtn.addEventListener('click', function(){
+    console.log("you've pressed the deal button");
+
+    // Create a new card image element
+    const newCard = document.createElement('img');
+    console.log(newCard);
+  })
 })
