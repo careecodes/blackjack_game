174bbbcc898362e44ea5d52c21a0a97c092e240a
Caree Youngman <caree@caree.codes>
2021-10-28 19:41:14 -0500

    simple deal button functionality

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,20 @@ window.addEventListener('DOMContentLoaded', function() {
   dealBtn.addEventListener('click', function(){
     console.log("you've pressed the deal button");
 
-    // Create a new card image element
+    // Create a new card image element for dealer
     const newCard = document.createElement('img');
-    console.log(newCard);
+    newCard.src = "images/2_of_clubs.png";
+
+    // Create new card image element for player
+    const playerCard = document.createElement('img');
+    playerCard.src = "images/4_of_diamonds.png";
+
+    // Append the new card image element to the dealer hand div
+    const dealerHand = document.querySelector("#dealer-hand");
+    dealerHand.appendChild(newCard);
+
+    // Append the player card to the player hand div
+    const playerHand = document.querySelector("#player-hand");
+    playerHand.appendChild(playerCard);
   })
 })
