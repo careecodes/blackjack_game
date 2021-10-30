7dd11b90d9a3c1577e156096e3b0bb1d5b7f9fff
Caree Youngman <caree@caree.codes>
2021-10-30 09:04:02 -0500

    Deck of cards generation

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,9 @@ window.addEventListener('DOMContentLoaded', function() {
     console.log("you've pressed the deal button");
 
     // Create a new card image element for dealer
+    const mySuit = "clubs";
     const newCard = document.createElement('img');
-    newCard.src = "images/2_of_clubs.png";
+    newCard.src = `images/2_of_${mySuit}.png`;
 
     // Create new card image element for player
     const playerCard = document.createElement('img');
@@ -20,5 +21,29 @@ window.addEventListener('DOMContentLoaded', function() {
     // Append the player card to the player hand div
     const playerHand = document.querySelector("#player-hand");
     playerHand.appendChild(playerCard);
-  })
+  });
+
+
+  let myDeck = [];
+
+const suits = [
+	"clubs",
+	"diamonds", 
+	"hearts",
+	"spades"
+];
+
+// loop through each of the 4 suits...
+for(s=0; s<suits.length; s++) {
+	// loop through rank 1-13
+	for(r=1; r<=13; r++){
+	myDeck.push(
+		{rank: r,
+		suit: suits[s]}
+	);
+	}
+}
+
+console.log(myDeck);
+console.log("total number of cards: " + myDeck.length);
 })
